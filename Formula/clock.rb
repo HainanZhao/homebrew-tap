bed37e74c34520c96101cbd2d00d145c8ca61988
Hainan Zhao <hainan.zhao@grasshopperasia.com>
2026-08-14 19:28:32 +0800

    Remove Google Calendar integration to reduce dependency size by 70%

--- Formula/clock.rb
+++ Formula/clock.rb
@@ -1,8 +1,8 @@
 class Clock < Formula
   desc "A highly customizable, gorgeous retro terminal clock with premium faces"
   homepage "https://github.com/HainanZhao/clock"
-  url "https://github.com/HainanZhao/clock/archive/af60f5782067351c08a379963cc034f8602ce162.tar.gz"
-  sha256 "cf80f1904293c3f492c4351c288ad2f6e7b39c0810bafc883156d6a0e0a16b30"
+  url "https://github.com/HainanZhao/clock/archive/6cfb3566e3672f6cf2f0d04063cbff5cab1bf32a.tar.gz"
+  sha256 "2f276db1b07e4d0d40a577cd626b16ecf03c910121d0ceea5a21b3d9d587ea30"
   license "MIT"
 
   head "https://github.com/HainanZhao/clock.git", branch: "main"
