04d7562f2824fa8564d6c3185621e0f95554936c
Hainan Zhao <hainan.zhao@grasshopperasia.com>
2026-08-14 19:00:11 +0800

    Smooth warp face refresh rate to 30 FPS

--- Formula/clock.rb
+++ Formula/clock.rb
@@ -1,8 +1,8 @@
 class Clock < Formula
   desc "A highly customizable, gorgeous retro terminal clock with premium faces"
   homepage "https://github.com/HainanZhao/clock"
-  url "https://github.com/HainanZhao/clock/archive/4231fdd520d889533bcc83506462b9972a095197.tar.gz"
-  sha256 "500b493f2bc468c21cc447d8076be43790c2e4585afa48c093e84f9ffe67ae6b"
+  url "https://github.com/HainanZhao/clock/archive/4e0f78710de79d2ed7b1e73d1ff733241ccd4e25.tar.gz"
+  sha256 "d94f48f18069f319467ef147b1816ed6f54631661b21f70610898523aa6016a9"
   license "MIT"
 
   head "https://github.com/HainanZhao/clock.git", branch: "main"
