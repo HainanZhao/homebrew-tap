6c15c2ad5195edc476be045ad1cbd7585edebca2
Hainan Zhao <hainan.zhao@grasshopperasia.com>
2026-08-14 18:55:47 +0800

    Update clock to support latest main and add warp face

--- Formula/clock.rb
+++ Formula/clock.rb
@@ -1,9 +1,11 @@
 class Clock < Formula
   desc "A highly customizable, gorgeous retro terminal clock with premium faces"
   homepage "https://github.com/HainanZhao/clock"
-  url "https://github.com/HainanZhao/clock/archive/refs/tags/v0.6.0.tar.gz"
-  sha256 "f21e23bb3e419a123a0f1a325d25fa4c52dacce1a48e55d4cbd3185728d01022"
+  url "https://github.com/HainanZhao/clock/archive/e5bba8b7bcc0f1bc28f85469a73fd49ee3c8a449.tar.gz"
+  sha256 "98b386e11e7fe5c1dce3a7438b6e640e35ea7ca9d445bfa88feee4ac81eccfd7"
   license "MIT"
+
+  head "https://github.com/HainanZhao/clock.git", branch: "main"
 
   depends_on "rust" => :build
 
