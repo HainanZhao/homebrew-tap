deb7551151cbb05f8ea6fada501e03682626d4de
hainzhao <hainzhao@gmail.com>
2026-08-31 05:36:50 +0000

    Update tictock to 0.7.2

--- Formula/tictock.rb
+++ Formula/tictock.rb
@@ -1,8 +1,8 @@
 class Tictock < Formula
   desc "Polished, configurable terminal clock with 18 distinctive faces"
   homepage "https://github.com/HainanZhao/tictock"
-  url "https://github.com/HainanZhao/tictock/archive/refs/tags/v0.7.1.tar.gz"
-  sha256 "1fea6491ed81662bbd6b1fc3da9c46afac319cbc2c4565c3c9b1aaba1d02e460"
+  url "https://github.com/HainanZhao/tictock/archive/refs/tags/v0.7.2.tar.gz"
+  sha256 "16eb58c230e53786dbd18d8c98b363b4e5a893a5bd7291fbd65d099f7e82c046"
   license "MIT"
 
   head "https://github.com/HainanZhao/tictock.git", branch: "main"
